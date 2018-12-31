78624c4e4b715de13b259d3b10756d23690f8e7a
Stefanus Prasetyo Nugroho <stefanusnugroho585@gmail.com>
2019-01-01 00:35:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Place Autocomplete and Directions</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/bootstrap4.min.css">
+
+    <!-- Include SmartWizard CSS -->
+    <link href="assets/css/smart_wizard.css" rel="stylesheet" type="text/css">
+
+    <!-- Optional SmartWizard theme -->
+    <link href="assets/css/smart_wizard_theme_circles.css" rel="stylesheet" type="text/css"  />
+
+    <link href="assets/css/smart_wizard_theme_arrows.css" rel="stylesheet" type="text/css"  />
+
+    <link href="assets/css/smart_wizard_theme_dots.css" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" type="text/css" href="style.css" >
+
+  </head>
+  <body>
+    <div class="container">
+        <br />
+        <form action="coba.php" id="myForm" role="form" data-toggle="validator" method="post" accept-charset="utf-8">
+
+        <!-- SmartWizard html -->
+        <div id="smartwizard">
+            <ul>
+                <li><a href="#step-1">Step 1<br /><small>Order Ojek Online</small></a></li>
+                <li><a href="#step-2">Step 2<br /><small>Catatan Pengemudi</small></a></li>
+                <li><a href="#step-3">Step 3<br /><small>Pembayaran</small></a></li>
+                <li><a href="#step-4">Step 4<br /><small>Selesai</small></a></li>
+            </ul>
+
+            <div>
+                <div id="step-1">
+                    <div id="right-panel"></div>
+                    <div id="map"></div>
+                    <img src="assets/images/gps.png" class="tombolgps" id="gunakanlokasi"/>
+                    <div id="mode-selector" class="controls">
+                  <input type="radio" name="type" id="changemode-walking" checked="checked">
+                  <label for="changemode-walking">Motorcycle And Walking</label>
+
+                  <input type="radio" name="type" id="changemode-transit">
+                  <label for="changemode-transit">Transit</label>
+
+                  <input type="radio" name="type" id="changemode-driving">
+                  <label for="changemode-driving">Car</label>
+
+                </div>
+                    <div id="form-step-0" role="form" data-toggle="validator">
+                        <div class="form-group">
+                          <label for="usr"></label>
+                          <input type="text" class="form-control" name="asal" id="asal" placeholder="Masukkan Lokasi Penjemputan">
+                        </div>
+                        <div class="form-group">
+                          <label for="pwd"></label>
+                          <input type="text" class="form-control" id="tujuan" name="tujuan" placeholder="Masukkan Lokasi Pengantaran">
+                        </div>
+                        <div class="form-group">
+                          <label for="usr"></label>
+                          <input type="text" class="form-control" id="distance" name="jarak" readonly>
+                        </div>
+                        <div class="form-group">
+                          <label for="pwd"></label>
+                          <input type="text" class="form-control" id="billing" name="tarif" readonly>
+                        </div>
+                    </div>
+
+                </div>
+                
+                <div id="step-2">
+                    <h2></h2>
+                    <div id="form-step-1" role="form" data-toggle="validator">
+                        <div class="form-group">
+                            <label for="name">Catatan Pengemudi</label>
+                            <input type="text" class="form-control" name="catatan" id="catatan" placeholder="Catatan Pengemudi" required>
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                </div>
+                <div id="step-3">
+                    <h2>Pilih Pembayaran</h2>
+                    <div id="form-step-2" role="form" data-toggle="validator">
+                        <div class="form-group">
+                            <input type="checkbox" name="pembayaran" value="Tunai">Tunai</input>
+                            
+                            <input type="checkbox" name="pembayaran" value="NonTunai">Non Tunai<br>
+
+                            <label for="name">Promo(Optional)</label>
+                            <input type="text" class="form-control" name="promo" id="promo" placeholder="Masukan Promo" required>
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+                </div>
+                <div id="step-4" class="">
+                    <h2>Silahkan Pilih Driver</h2>
+                    <div id="form-step-3" role="form" data-toggle="validator">
+                        <div class="form-group">
+                            <input type="checkbox" name="driver" value="Si Cantik">
+                            <img src="assets/images/sicantik.jpg"/ class="driverimg"></input>
+                            <p style="float: right; margin-right: 70%;">Si Cantik<br>No Pol B 1234 ABC</p>
+                            <br><br>
+                            <input type="checkbox" name="driver" value="Bang Soplo">
+                            <img src="assets/images/bang soplo.jpg"/ class="driverimg"></input>
+                            <p style="float: right; margin-right: 70%;">Bang Soplo<br>No Pol B 4567 FGC</p>
+                            <br><br>
+                             <input type="checkbox" name="driver" value="Bang Toyib">
+                            <img src="assets/images/kartono.png"/ class="driverimg"></input>
+                            <p style="float: right; margin-right: 70%;">Bang Toyib<br>No Pol B 3456 SKK</p>
+                            <br><br>
+                            <input type="checkbox" name="driver" value="Della Putriara">
+                            <img src="assets/images/Della Putriara.jpg"/ class="driverimg"></input>
+                            <p style="float: right; margin-right: 70%;">Della Putriara<br>No Pol B 7890 FVJ</p>
+                            <br><br>
+                            <div class="help-block with-errors"></div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+        </form>
+
+    </div>
+   
+<!-- jQuery library -->
+<script src="assets/js/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="assets/js/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="assets/js/bootstrap4.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.smartWizard.min.js"></script>
+<script type="text/javascript">
+        $(document).ready(function(){
+
+            // Step show event
+            
+
+            // Toolbar extra buttons
+            var btnFinish = 
+            $('<button></button>').text('Finish').addClass('btn btn-info')
+                .on('click', function(){ 
+                  alert('Finish Order');
+                  window.location.href="detailorder.php";
+                   });
+
+            var btnCancel = 
+            $('<button></button>').text('Cancel').addClass('btn btn-danger').on('click', function(){ 
+                $('#smartwizard').smartWizard("reset"); 
+            });
+
+            // Please note enabling option "showStepURLhash" will make navigation conflict for multiple wizard in a page.
+            // so that option is disabling => showStepURLhash: false
+
+            // Smart Wizard 1
+            $('#smartwizard').smartWizard({
+                    selected: 0,
+                    theme: 'arrows',
+                    transitionEffect:'fade',
+                    showStepURLhash: false,
+                    toolbarSettings: 
+                    {
+                        toolbarPosition: 'bottom',
+                        toolbarExtraButtons: [btnFinish, btnCancel]
+                    }
+            });
+
+            
+        });
+    </script>
+    
+    <script src="mapsDefault.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRMGD9yRbxXALm2n_hjCbkZmbhB9yMLkk&libraries=places&callback=initMap"
+        async defer></script>
+          </body>
+</html>
---
+++ mapsDefault.js
@@ -0,0 +1,211 @@
+ // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+ // Sumber :
+ // https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-directions
+ // http://findnerd.com/list/view/Calculate-distance-between-two-addresses--points--locations-using-Google-map-API-/26285/
+
+      var infoWindow;
+      function initMap() {
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          mapTypeControl: false,
+          center: {lat: -6.232934, lng: 107.086914},
+          animation: google.maps.Animation.BOUNCE,
+          zoom: 13,
+
+        });
+        var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+        var bikeLayer = new google.maps.BicyclingLayer();
+        bikeLayer.setMap(map);
+        new AutocompleteDirectionsHandler(map);
+
+        $('#gunakanlokasi').click(function(){
+          infoWindow = new google.maps.InfoWindow;
+            // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            var marker = new google.maps.Marker({
+                  position: pos,
+                  map: map,
+                  draggable:true,
+                  title: "Anda Disini"
+              });
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+ 
+        });
+
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+       /**
+        * @constructor
+       */
+      function AutocompleteDirectionsHandler(map) {
+        this.map = map;
+        this.originPlaceId = null;
+        this.destinationPlaceId = null;
+        this.travelMode = 'DRIVING';
+        var originInput = document.getElementById('asal');
+        var destinationInput = document.getElementById('tujuan');
+        var modeSelector = document.getElementById('mode-selector');
+        this.directionsService = new google.maps.DirectionsService;
+        this.directionsDisplay = new google.maps.DirectionsRenderer;
+        //this.directionsDisplay.draggable = true;
+        this.directionsDisplay.setMap(map);
+
+        var originAutocomplete = new google.maps.places.Autocomplete(
+            originInput, {placeIdOnly: true});
+        var destinationAutocomplete = new google.maps.places.Autocomplete(
+            destinationInput, {placeIdOnly: true});
+
+        this.setupClickListener('changemode-walking', 'WALKING');
+        this.setupClickListener('changemode-transit', 'TRANSIT');
+        this.setupClickListener('changemode-driving', 'DRIVING');
+
+        this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+        this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+        //this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(asal);
+        //this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(tujuan);
+        this.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(modeSelector);
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(gunakanlokasi);
+      }
+
+      // Sets a listener on a radio button to change the filter type on Places
+      // Autocomplete.
+      AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
+        var radioButton = document.getElementById(id);
+        var me = this;
+        radioButton.addEventListener('click', function() {
+          me.travelMode = mode;
+          me.route();
+        });
+      };
+
+      AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+        var me = this;
+        autocomplete.bindTo('bounds', this.map);
+        autocomplete.addListener('place_changed', function() {
+          var place = autocomplete.getPlace();
+          if (!place.place_id) {
+            window.alert("Please select an option from the dropdown list.");
+            return;
+          }
+          if (mode === 'ORIG') {
+            me.originPlaceId = place.place_id;
+          } else {
+            me.destinationPlaceId = place.place_id;
+          }
+          me.route();
+        });
+
+      };
+
+      AutocompleteDirectionsHandler.prototype.route = function() {
+        if (!this.originPlaceId || !this.destinationPlaceId) {
+          return;
+        }
+        var me = this;
+
+        this.directionsService.route({
+          origin: {'placeId': this.originPlaceId},
+          destination: {'placeId': this.destinationPlaceId},
+          travelMode: this.travelMode
+        }, function(response, status) {
+          if (status === 'OK') {
+            me.directionsDisplay.setDirections(response);
+            var service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix({
+        origins: [document.getElementById("asal").value],
+        destinations: [document.getElementById("tujuan").value],
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: true
+    }, function (response, status) {
+        if (status == google.maps.DistanceMatrixStatus.OK && response.rows[0].elements[0].status != "ZERO_RESULTS") {
+            var distance = response.rows[0].elements[0].distance.text;
+            var duration = response.rows[0].elements[0].duration.text;
+            var dvDistance = document.getElementById("distance");
+           dvDistance.value = "";
+           $( "#distance" ).prop( "readonly", true );
+           $("#billing").prop("readonly",true);
+            dvDistance.value += distance + "dan" + "Waktu Tempuh" +duration;
+            //dvDistance.innerHTML += "Duration:" + duration;
+             var str = distance;
+          var distance1 = str.replace(' km', '');
+          var distance2 = distance1.replace(',','.');
+          var d = new Date();
+          /*          
+          
+          /*          
+            jumlah kilometer dikalikan dengan 500 
+            setelah itu hasilnya kita konversikan kedalam format kurs rupiah
+          */
+          if (distance2 >=0 && distance2<= 5){
+            var tarif =  formatNumber(8000);  
+          document.getElementById("billing").value = tarif;
+
+          }
+          else if (distance2 <= 100,0){
+            var tarif =  formatNumber(distance2*6000);  
+          document.getElementById("billing").value = tarif;
+
+          }
+          else if(d.getHours()>=6 && d.getHours()<= 9){
+            var tarif =  formatNumber(distance2 * 2000); 
+          document.getElementById("billing").value = tarif;
+
+          }
+          else if(d.getHours()>=16 && d.getHours()<= 20){
+            var tarif = formatNumber(distance2 * 2000); 
+          document.getElementById("billing").value = tarif;
+
+          }
+          else{
+          var tarif = formatNumber(distance2 * 1700); 
+          document.getElementById("billing").value = tarif;
+        }
+          
+        } else {
+            alert("Unable to find the distance via road.");
+        }
+    });
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      };
+
+      /*      
+    menghitung jarak dari data yg dikirim dari form
+    disini saya setting untuk mode DRIVING dan menggunakan jalan raya atau juga tol,
+    jika ingin mengganti konfigurasinya, silahkan ganti false dengan true
+    */
+      
+      // fungsi sederhana untuk mengkonversi bilangan bulat menjadi format kurs rupiah
+    function formatNumber (angka) {
+      var rupiah = '';
+    var angkarev = angka.toString().split('').reverse().join('');
+    for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
+    return rupiah.split('',rupiah.length-1).reverse().join('');
+    }
+
